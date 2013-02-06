0c411bcdd3557a5cefd0e744ecd3567bdb5ef42a
Frank Lyder Bredland <fl@nyggen.com>
2013-02-06 17:57:22 +0100

    did some changes

--- PlanetCute_ida/PlanetCute_ida/PlanetCute_ida/Background.cs
+++ PlanetCute_ida/PlanetCute_ida/PlanetCute_ida/Background.cs
@@ -26,6 +26,12 @@ namespace PlanetCute_ida
         Texture2D DoorTallClosed;
         Texture2D StoneBlock;
 
+        Texture2D[] sprites = new Texture2D[13];
+
+        const int mapWidth  = 7;
+        const int mapHeight = 5;
+        int[,] map = new int[mapWidth, mapHeight];
+
         public Background(Texture2D RoofNorthWest, Texture2D RoofNorth,
             Texture2D RoofNorthEast, Texture2D RoofWest, Texture2D BrownBlock,
             Texture2D RoofEast, Texture2D RoofSouthWest, Texture2D RoofSouth,
@@ -49,37 +55,81 @@ namespace PlanetCute_ida
 
         public Background(ContentManager Content)
         {
-            this.RoofNorthWest = Content.Load<Texture2D>(@"images/Roof North West");
-            this.RoofNorth = Content.Load<Texture2D>(@"images/Roof North");
-            this.RoofNorthEast = Content.Load<Texture2D>(@"images/Roof North East");
-            this.RoofWest = Content.Load<Texture2D>(@"images/Roof West");
-            this.BrownBlock = Content.Load<Texture2D>(@"images/Brown Block");
-            this.RoofEast = Content.Load<Texture2D>(@"images/Roof East");
-            this.RoofSouthWest = Content.Load<Texture2D>(@"images/Roof South West");
-            this.RoofSouth = Content.Load<Texture2D>(@"images/Roof South");
-            this.WindowTall = Content.Load<Texture2D>(@"images/Window Tall");
-            this.RoofSouthEast = Content.Load<Texture2D>(@"images/Roof South East");
-            this.WallBlockTall = Content.Load<Texture2D>(@"images/Wall Block Tall");
-            this.DoorTallClosed = Content.Load<Texture2D>(@"images/Door Tall Closed");
-            this.StoneBlock = Content.Load<Texture2D>(@"images/Stone Block");
+            this.sprites[0] = Content.Load<Texture2D>(@"images/Roof North West");
+            this.sprites[1] = Content.Load<Texture2D>(@"images/Roof North");
+            this.sprites[2] = Content.Load<Texture2D>(@"images/Roof North East");
+            this.sprites[3] = Content.Load<Texture2D>(@"images/Roof West");
+            this.sprites[4] = Content.Load<Texture2D>(@"images/Brown Block");
+            this.sprites[5] = Content.Load<Texture2D>(@"images/Roof East");
+            this.sprites[6] = Content.Load<Texture2D>(@"images/Roof South West");
+            this.sprites[7] = Content.Load<Texture2D>(@"images/Roof South");
+            this.sprites[8] = Content.Load<Texture2D>(@"images/Window Tall");
+            this.sprites[9] = Content.Load<Texture2D>(@"images/Roof South East");
+            this.sprites[10] = Content.Load<Texture2D>(@"images/Wall Block Tall");
+            this.sprites[11] = Content.Load<Texture2D>(@"images/Door Tall Closed");
+            this.sprites[12] = Content.Load<Texture2D>(@"images/Stone Block");
+
+            this.map[0, 0] = 0;
+            this.map[0, 1] = 3;
+            this.map[0, 2] = 6;
+            this.map[0, 3] = 10;
+            this.map[0, 4] = 12;
+
+            for (int i = 1; i < 5; i++)
+            {
+                this.map[i, 0] = 1;
+                this.map[i, 1] = 1;
+                this.map[i, 2] = 7;
+                this.map[i, 3] = 10;
+                this.map[i, 4] = 12;
+            }
+
+            this.map[5, 0] = 1;
+            this.map[5, 1] = 4;
+            this.map[5, 2] = 8;
+            this.map[5, 3] = 11;
+            this.map[5, 4] = 12;
         }
 
         public void Update()
         {
         }
 
+        public int getY(int y)
+        {
+            int[] arr = {4, 3, 0, 1, 2};
+            return arr[y];
+        }
+
         public void Draw(SpriteBatch spriteBatch)
         {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                for (int y = 0; y < mapHeight; y++)
+                {
+                    int tmpY = getY(y);
+                    Texture2D sprite = this.sprites[map[x, tmpY]];
+
+                    spriteBatch.Draw(
+                                        sprite,
+                                        new Vector2(
+                                                    sprite.Width * x,
+                                                    sprite.Height * tmpY / 2
+                                                   ),
+                                        Color.White);
+                }
+            }
+            /*
             //Floor
 
 
             for (int i = 0; i < 7; i++)
             {
-                spriteBatch.Draw(StoneBlock,
-                    new Vector2(
-                        (StoneBlock.Width * i),
-                        (WallBlockTall.Height * 2) + (RoofNorth.Height / 6)),
-                        Color.White);
+                spriteBatch.Draw(   StoneBlock,
+                                    new Vector2(
+                                        (StoneBlock.Width * i),
+                                        (WallBlockTall.Height * 2) + (RoofNorth.Height / 6)),
+                                   Color.White);
             }
 
             for (int i = 0; i < 5; i++)
@@ -162,6 +212,7 @@ namespace PlanetCute_ida
                     (RoofSouth.Width * 6),
                     (RoofNorth.Height)),
                     Color.White);
+             * */
         }
     }
 }
