0a5768e7302cae4f4b52f3f4becda3fcc8c17df8
Frank Lyder Bredland <fl@nyggen.com>
2013-02-06 18:28:29 +0100

    Got background finally working as I wanted

--- PlanetCute_ida/PlanetCute_ida/PlanetCute_ida/Background.cs
+++ PlanetCute_ida/PlanetCute_ida/PlanetCute_ida/Background.cs
@@ -12,63 +12,31 @@ namespace PlanetCute_ida
 {
     class Background
     {
-        Texture2D RoofNorthWest;
-        Texture2D RoofNorth;
-        Texture2D RoofNorthEast;
-        Texture2D RoofWest;
-        Texture2D BrownBlock;
-        Texture2D RoofEast;
-        Texture2D RoofSouthWest;
-        Texture2D RoofSouth;
-        Texture2D WindowTall;
-        Texture2D RoofSouthEast;
-        Texture2D WallBlockTall;
-        Texture2D DoorTallClosed;
-        Texture2D StoneBlock;
-
-        Texture2D[] sprites = new Texture2D[13];
-
+        Tile[] tSprites = new Tile[14];
+        
         const int mapWidth  = 7;
         const int mapHeight = 5;
         int[,] map = new int[mapWidth, mapHeight];
 
-        public Background(Texture2D RoofNorthWest, Texture2D RoofNorth,
-            Texture2D RoofNorthEast, Texture2D RoofWest, Texture2D BrownBlock,
-            Texture2D RoofEast, Texture2D RoofSouthWest, Texture2D RoofSouth,
-            Texture2D WindowTall, Texture2D RoofSouthEast, Texture2D WallBlockTall,
-            Texture2D DoorTallClosed, Texture2D StoneBlock)
-        {
-            this.RoofNorthWest = RoofNorthWest;
-            this.RoofNorth = RoofNorth;
-            this.RoofNorthEast = RoofNorthEast;
-            this.RoofWest = RoofWest;
-            this.BrownBlock = BrownBlock;
-            this.RoofEast = RoofEast;
-            this.RoofSouthWest = RoofSouthWest;
-            this.RoofSouth = RoofSouth;
-            this.WindowTall = WindowTall;
-            this.RoofSouthEast = RoofSouthEast;
-            this.WallBlockTall = WallBlockTall;
-            this.DoorTallClosed = DoorTallClosed;
-            this.StoneBlock = StoneBlock;
-        }
-
         public Background(ContentManager Content)
         {
-            this.sprites[0] = Content.Load<Texture2D>(@"images/Roof North West");
-            this.sprites[1] = Content.Load<Texture2D>(@"images/Roof North");
-            this.sprites[2] = Content.Load<Texture2D>(@"images/Roof North East");
-            this.sprites[3] = Content.Load<Texture2D>(@"images/Roof West");
-            this.sprites[4] = Content.Load<Texture2D>(@"images/Brown Block");
-            this.sprites[5] = Content.Load<Texture2D>(@"images/Roof East");
-            this.sprites[6] = Content.Load<Texture2D>(@"images/Roof South West");
-            this.sprites[7] = Content.Load<Texture2D>(@"images/Roof South");
-            this.sprites[8] = Content.Load<Texture2D>(@"images/Window Tall");
-            this.sprites[9] = Content.Load<Texture2D>(@"images/Roof South East");
-            this.sprites[10] = Content.Load<Texture2D>(@"images/Wall Block Tall");
-            this.sprites[11] = Content.Load<Texture2D>(@"images/Door Tall Closed");
-            this.sprites[12] = Content.Load<Texture2D>(@"images/Stone Block");
+            // Loading in sprites
+            this.tSprites[0]  = new Tile(Content, @"images/Roof North West",    0);
+            this.tSprites[1]  = new Tile(Content, @"images/Roof North",         0);
+            this.tSprites[2]  = new Tile(Content, @"images/Roof North East",    0);
+            this.tSprites[3]  = new Tile(Content, @"images/Roof West",          0);
+            this.tSprites[4]  = new Tile(Content, @"images/Brown Block",        0);
+            this.tSprites[5]  = new Tile(Content, @"images/Roof East",          0);
+            this.tSprites[6]  = new Tile(Content, @"images/Roof South West",    0);
+            this.tSprites[7]  = new Tile(Content, @"images/Roof South",         0);
+            this.tSprites[8]  = new Tile(Content, @"images/Window Tall",        0);
+            this.tSprites[9]  = new Tile(Content, @"images/Roof South East",    0);
+            this.tSprites[10] = new Tile(Content, @"images/Wall Block Tall",    0);
+            this.tSprites[11] = new Tile(Content, @"images/Door Tall Closed",   20);
+            this.tSprites[12] = new Tile(Content, @"images/Stone Block",        20);
+            this.tSprites[13] = new Tile(Content, @"images/Roof North",         -40);
 
+            // Setting up the map
             this.map[0, 0] = 0;
             this.map[0, 1] = 3;
             this.map[0, 2] = 6;
@@ -78,141 +46,40 @@ namespace PlanetCute_ida
             for (int i = 1; i < 5; i++)
             {
                 this.map[i, 0] = 1;
-                this.map[i, 1] = 1;
+                this.map[i, 1] = 4;
                 this.map[i, 2] = 7;
                 this.map[i, 3] = 10;
                 this.map[i, 4] = 12;
             }
 
             this.map[5, 0] = 1;
-            this.map[5, 1] = 4;
+            this.map[5, 1] = 13;
             this.map[5, 2] = 8;
             this.map[5, 3] = 11;
             this.map[5, 4] = 12;
-        }
 
-        public void Update()
-        {
-        }
-
-        public int getY(int y)
-        {
-            int[] arr = {4, 3, 0, 1, 2};
-            return arr[y];
+            this.map[6, 0] = 2;
+            this.map[6, 1] = 5;
+            this.map[6, 2] = 9;
+            this.map[6, 3] = 10;
+            this.map[6, 4] = 12;
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
+            int[] arr = { 4, 3, 0, 1, 2 };
+            
             for (int x = 0; x < mapWidth; x++)
             {
                 for (int y = 0; y < mapHeight; y++)
                 {
-                    int tmpY = getY(y);
-                    Texture2D sprite = this.sprites[map[x, tmpY]];
+                    int tmpY = arr[y];
+                    Tile sprite = this.tSprites[map[x, tmpY]];
 
-                    spriteBatch.Draw(
-                                        sprite,
-                                        new Vector2(
-                                                    sprite.Width * x,
-                                                    sprite.Height * tmpY / 2
-                                                   ),
-                                        Color.White);
+                    // Drawing each tile
+                    sprite.Draw(spriteBatch, x, tmpY);
                 }
             }
-            /*
-            //Floor
-
-
-            for (int i = 0; i < 7; i++)
-            {
-                spriteBatch.Draw(   StoneBlock,
-                                    new Vector2(
-                                        (StoneBlock.Width * i),
-                                        (WallBlockTall.Height * 2) + (RoofNorth.Height / 6)),
-                                   Color.White);
-            }
-
-            for (int i = 0; i < 5; i++)
-            {
-                spriteBatch.Draw(WallBlockTall,
-                    new Vector2(
-                        (WallBlockTall.Width * i),
-                        (WallBlockTall.Height) + (RoofNorth.Height / 2)),
-                        Color.White);
-            }
-            spriteBatch.Draw(DoorTallClosed,
-                new Vector2(
-                    (WallBlockTall.Width * 5),
-                    (WindowTall.Height) + (RoofNorth.Height / 2) + 20),
-                    Color.White);
-            spriteBatch.Draw(WallBlockTall,
-                new Vector2(
-                    (WallBlockTall.Width * 6),
-                    (WallBlockTall.Height) + (RoofNorth.Height / 2)),
-                    Color.White);
-            //Roof
-            spriteBatch.Draw(RoofNorthWest, Vector2.Zero, Color.White);
-            for (int i = 1; i < 6; i++)
-            {
-                spriteBatch.Draw(RoofNorth,
-                    new Vector2(
-                        (RoofNorth.Width * i),
-                        (0)),
-                        Color.White);
-            }
-            spriteBatch.Draw(RoofNorthEast,
-                new Vector2(
-                    (RoofNorthEast.Width * 6),
-                    (0)),
-                    Color.White);
-            spriteBatch.Draw(RoofWest,
-                new Vector2(
-                    (0),
-                    (RoofNorthWest.Height / 2)),
-                    Color.White);
-            for (int i = 1; i < 5; i++)
-            {
-                spriteBatch.Draw(BrownBlock,
-                    new Vector2(
-                        (BrownBlock.Width * i),
-                        (RoofNorthWest.Height / 2)),
-                        Color.White);
-            }
-
-            spriteBatch.Draw(RoofNorth,
-                new Vector2(
-                    (RoofNorth.Width * 5),
-                    (RoofNorth.Height / 3) - 10),
-                    Color.White);
-            spriteBatch.Draw(RoofEast,
-                new Vector2(
-                    (RoofNorth.Width * 6),
-                    (RoofNorth.Height / 2)),
-                    Color.White);
-            spriteBatch.Draw(RoofSouthWest,
-                new Vector2(
-                    (0),
-                    (RoofNorth.Height)),
-                    Color.White);
-            for (int i = 1; i < 5; i++)
-            {
-                spriteBatch.Draw(RoofSouth,
-                    new Vector2(
-                        (RoofSouth.Width * i),
-                        (RoofNorth.Height)),
-                        Color.White);
-            }
-            spriteBatch.Draw(WindowTall,
-                new Vector2(
-                    (RoofSouth.Width * 5),
-                    (RoofNorth.Height)),
-                    Color.White);
-            spriteBatch.Draw(RoofSouthEast,
-                new Vector2(
-                    (RoofSouth.Width * 6),
-                    (RoofNorth.Height)),
-                    Color.White);
-             * */
         }
     }
 }
---
+++ PlanetCute_ida/PlanetCute_ida/PlanetCute_ida/Tile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace PlanetCute_ida
+{
+    class Tile
+    {
+        Texture2D sprite;
+        int offsetX;
+
+        public Tile(ContentManager Content, String name, int offset)
+        {
+            this.sprite = Content.Load<Texture2D>(name);
+            this.offsetX = offset;
+        }
+
+        public void Draw(SpriteBatch spriteBatch, int x, int y)
+        {
+            spriteBatch.Draw(   sprite,
+                                new Vector2(
+                                            sprite.Width * x,
+                                            sprite.Height * y / 2 + this.offsetX
+                                            ),
+                                Color.White);
+        }
+    }
+}
